bb1ce15f9d87a3ca1697f868520994f1e4ec2886
Matz-Mp4 <mateus.assalti@gmail.com>
2023-06-03 16:26:36 -0300

    Fixed mason problem

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -4,6 +4,9 @@ if not status_ok then
 end
 
 -- configure mason and it's LSP integration (provides the :LspInstall command)
+require("user.lsp.handlers").setup()
+
+require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
   -- This is a default handler that will be called for each installed server (also for new servers that are installed during a session)
@@ -14,6 +17,5 @@ require("mason-lspconfig").setup_handlers {
     end
   end,
 }
-require("user.lsp.handlers").setup()
 --[[ require("user.lsp.lsp-signature") ]]
 require("user.lsp.lsp-saga");
